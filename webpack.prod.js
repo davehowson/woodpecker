c691bc8ca3657201582a2099f274267a0a4e55ff
Dave Howson <dave.sohan@gmail.com>
2019-06-05 18:42:30 +0530

    Removed compression

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
-const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = merge(common, {
     output: {
@@ -10,9 +9,4 @@ module.exports = merge(common, {
         publicPath: '/'
     },
     mode: 'production',
-    plugins: [
-        new CompressionPlugin({
-            test: /\.(js|css|html|svg)$/,
-        })
-    ]
 });
